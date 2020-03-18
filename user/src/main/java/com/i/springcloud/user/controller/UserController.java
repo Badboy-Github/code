346f97e64a30ae495e8ea3b12539bd73c856d9dd
chenlin <962230880@qq.com>
2020-03-18 15:42:26 +0800

    新增slf4j和maven

--- user/src/main/java/com/i/springcloud/user/controller/UserController.java
+++ user/src/main/java/com/i/springcloud/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.i.springcloud.user.controller;
 
 import com.i.springcloud.user.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("getUserById")
     public Object getUserById(Integer id){
+        logger.info("info");
+        logger.error("error ss");
         return userService.getUserById(id);
     }
 }
