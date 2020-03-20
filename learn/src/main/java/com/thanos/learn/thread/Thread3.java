8e0fed3239e9c6f064e1c95f6b0c91faea54ea60
chenlin <962230880@qq.com>
2020-03-20 16:50:25 +0800

    修改代码

---
+++ learn/src/main/java/com/thanos/learn/thread/Thread3.java
@@ -0,0 +1,14 @@
+package com.thanos.learn.thread;
+
+import java.util.concurrent.Callable;
+
+// V 泛型
+public class Thread3<V> implements Callable<V> {
+
+    @Override
+    public V call() throws Exception {
+        System.out.println("I am Tread3.");
+        return (V) "ok";
+    }
+
+}
--- learn/src/main/java/com/thanos/learn/thread/ThreadDemo.java
+++ learn/src/main/java/com/thanos/learn/thread/ThreadDemo.java
@@ -1,12 +1,13 @@
 package com.thanos.learn.thread;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadDemo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         System.out.println("ThreadDemo begin");
         // 实现Runnable的Thread1
         Thread thread1 = new Thread(new Thread1());
@@ -25,6 +26,14 @@ public class ThreadDemo {
         // isDaemon设置为true则为守护线程
         Timer timer = new Timer();
         timer.schedule(task1,date);
+        // 实现Callable的Thread3
+        Callable<String> callable = new Thread3<String>();
+        //将任务放进FutureTask里
+        FutureTask<String> futureTask = new FutureTask<String>(callable);
+        Thread thread3 = new Thread(futureTask);
+        thread3.start();
+        System.out.println(futureTask.get());
+        TimeUnit.SECONDS.sleep(1);
         System.out.println("ThreadDemo end");
     }
 
